b9e4eb9042e661b8b031a4036c9567434db76d1a
datata <dani@danitarazona.dev>
2023-06-28 12:16:02 +0200

    feat: Home to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import Home from "./pages/Home/Home";
 import './App.css'
 
 function App() {
-
-  const Home = () => <h1>Bienvenido</h1>;
-  const Contact = () => <h1>Contacto</h1>
+  const Contact = () => <h1>Contacto</h1>;
 
   return ( 
     <>
