d0b0111820d9854d256170ee096ef7288862334e
datata <dani@danitarazona.dev>
2023-06-28 12:50:53 +0200

    feat: create routes with react reouter dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Project from "./pages/Projects/Project";
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
 
   return ( 
     <>
-      <Home />
-      <Contact />      
-      <Project />
+      <Routes>
+        <Route path="*" element={<h1>404</h1>} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/projects" element={<Project />}/>
+      </Routes>
     </>  
   )
 }
